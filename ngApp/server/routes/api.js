61e34f34e3e0441e7dd18fe97fb2e671d91a4f83
bmshamsnahid <morisrenar@gmail.com>
2017-05-04 20:22:43 +0600

    database connected successfully

--- ngApp/server/routes/api.js
+++ ngApp/server/routes/api.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+var db = "localhost:27017/videoplayer";
+mongoose.Promise = global.Promise;
+
+mongoose.connect(db, (err) => {
+  if(err) {
+    console.log("error in connecting database: " + err);
+  } else {
+    console.log("Database connected successfully: " + db);
+  }
+});
 
 router.get('/', (req, res) => {
    res.send('api works');
