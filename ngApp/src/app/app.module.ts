aa0786a16f2bd2daea9a5861bf48c59a93084c64
bmshamsnahid <morisrenar@gmail.com>
2017-05-05 10:47:25 +0600

    fetch and play videos in iframe

--- ngApp/src/app/app.module.ts
+++ ngApp/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+  import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -9,6 +9,7 @@ import { HomeComponent } from './home/home.component';
 import { VideoCenterComponent } from './video-center/video-center.component';
 import { VideoListComponent } from './video-list/video-list.component';
 import { VideoDetailComponent } from './video-detail/video-detail.component';
+import { SafePipe } from './safe.pipe';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { VideoDetailComponent } from './video-detail/video-detail.component';
     HomeComponent,
     VideoCenterComponent,
     VideoListComponent,
-    VideoDetailComponent
+    VideoDetailComponent,
+    SafePipe
   ],
   imports: [
     BrowserModule,
