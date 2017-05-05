9e76a2b952a9db06c6c6673faebd5f35ae9e1248
bmshamsnahid <morisrenar@gmail.com>
2017-05-05 10:14:09 +0600

    fetch videos from the database

--- ngApp/src/app/video-center/video-center.component.ts
+++ ngApp/src/app/video-center/video-center.component.ts
@@ -1,25 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import {Video} from "../video";
+import {VideoService} from "../video.service";
 
 @Component({
   selector: 'app-video-center',
   templateUrl: './video-center.component.html',
-  styleUrls: ['./video-center.component.css']
+  styleUrls: ['./video-center.component.css'],
+  providers: [VideoService]
 })
 export class VideoCenterComponent implements OnInit {
 
-  videos: Video[] = [
-    { "_id": "1", "title": "Title 1", "url": "url 1", "description": "desc 1"},
-    { "_id": "2", "title": "Title 2", "url": "url 2", "description": "desc 2"},
-    { "_id": "3", "title": "Title 3", "url": "url 3", "description": "desc 3"},
-    { "_id": "4", "title": "Title 4", "url": "url 4", "description": "desc 4"}
-  ];
+  videos: Array<Video>;
 
   selectedVideo: Video;
 
-  constructor() { }
+  constructor(private _videoService: VideoService) { }
 
   ngOnInit() {
+    this._videoService.getVideos().subscribe(resVideoData => this.videos = resVideoData);
   }
 
   onSelectVideo(video: any) {
